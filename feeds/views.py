bb5b287a65a73b0dd1c9f95c076e08920337be08
pessi-v <pessimo@posteo.net>
2024-11-24 21:04:31 +0100

    add reset.css, add 12-column grid layout, add code to give article cards an index as a string

--- feeds/views.py
+++ feeds/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import FeedItem
+import inflect
 
 def home_view(request):
+    i = inflect.engine()
+
     feed_items = FeedItem.objects.order_by('-published_date')[:50]
-    return render(request, 'home.html', {'feed_items': feed_items})
+    feed_items_with_index_string = [(i.number_to_words(index), item) for index, item in enumerate(feed_items, start = 1)]
+    return render(request, 'home.html', {'feed_items': feed_items_with_index_string})
