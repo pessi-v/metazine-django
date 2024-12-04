1e15b41fce2f2e7d104b8c6fd9d9ce3b50724cf4
pessi-v <pessimo@posteo.net>
2024-12-04 01:04:32 +0100

    adjust h2 size, clean html tags and decimal code from titles

--- feeds/tasks.py
+++ feeds/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 from django.utils import timezone
 from .models import RSSFeed, FeedItem
 from .utils import extract_ogp_metadata
+import re
+import html
 
 @shared_task
 def fetch_rss_feeds():
@@ -21,7 +23,7 @@ def fetch_rss_feeds():
 
                 FeedItem.objects.create(
                     feed=feed,
-                    title=entry.title,
+                    title=decode_html_symbols(strip_html_tags(entry.title)),
                     link=entry.link,
                     description=entry.get('summary', ''),
                     published_date=timezone.now(),
@@ -32,3 +34,10 @@ def fetch_rss_feeds():
         
         feed.last_fetched = timezone.now()
         feed.save()
+
+def strip_html_tags(text):
+    clean = re.sub(r'<[^>]+>', '', text)
+    return clean
+
+def decode_html_symbols(text):
+    return html.unescape(text)
--- run_production.sh
+++
@@ -1,5 +0,0 @@
-    
-#!/bin/bash
-source environment/bin/activate
-python3 manage.py migrate
-nohup python3 manage.py runserver 0.0.0.0:8000
